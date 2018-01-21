bbdd97517bdf4f4dcb2530517a617213f97cfe20
Akash Karthick <karthickakash17@gmail.com>
2018-01-21 13:16:57 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function navigationMenu() {
+    var items = document.getElementsByClassName("navigation");
+    for (var i = 0; i < items.length; i++) {
+        items[i].classList.toggle("display-none");
+        items[i].classList.toggle("nav-animation");
+    }
+
+    var itemsa = document.getElementsByClassName("article");
+    for (var i = 0; i < itemsa.length; i++) {
+        itemsa[i].classList.toggle("article-animation");
+    }
+
+
+
+}
+
+document.getElementById("navigation").addEventListener('click', function(event) {
+    console.log(event.target);
+});
